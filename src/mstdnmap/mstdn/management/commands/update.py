8cc720f3be21284e1fd4bf1c1ff2b4ae41d082b3
chibiegg <chibiegg@chibiegg.net>
2017-04-16 03:38:55 +0900

    Parallel processing

--- src/mstdnmap/mstdn/management/commands/update.py
+++ src/mstdnmap/mstdn/management/commands/update.py
@@ -9,7 +9,7 @@ from mstdnmap.mstdn.models import Instance
 logger = logging.getLogger(__name__)
 
 
-def handle_instance(instance):
+def get_connected_hostname(instance):
     try:
         timeline = instance.get_timeline()
     except KeyboardInterrupt:
@@ -18,7 +18,7 @@ def handle_instance(instance):
         logger.warn("Cannot get timeline: %s", instance.hostname)
         return []
 
-    logger.warn("%s", instance.hostname)
+    logger.warn("get %s", instance.hostname)
 
     instance_hostnames = set()
 
@@ -30,11 +30,13 @@ def handle_instance(instance):
         hostname = account.split("@")[1]
         instance_hostnames.add(hostname)
 
-    instance_hostnames = list(instance_hostnames)
+    return list(instance_hostnames)
 
+
+def update_connections(instance, connected_hostnames):
     created_instances = []
     instances = []
-    for hostname in instance_hostnames:
+    for hostname in connected_hostnames:
         target_instance, created = Instance.objects.get_or_create(hostname=hostname)
         instances.append(target_instance)
         if created:
@@ -42,22 +44,31 @@ def handle_instance(instance):
 
     instance.connections.add(*instances)
     instance.save()
-
     return created_instances
+
+
+def handle_instance(instance):
+    return update_connections(instance, get_connected_hostname(instance))
+
+
+def pool_func(instance):
+    return (instance, get_connected_hostname(instance))
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         created_instances = []
-        for instance in Instance.objects.all():
-            created_instances += handle_instance(instance)
+
+        pool = Pool(50)
+        for i in pool.map(pool_func, Instance.objects.all()):
+            created_instances += update_connections(i[0], i[1])
 
         while True:
             created_instances = []
             instances = created_instances[:]
-            for instance in instances:
-                created_instances += handle_instance(instance)
+            for i in pool.map(pool_func, instances):
+                created_instances += update_connections(i[0], i[1])
 
             if len(created_instances) == 0:
                 break
